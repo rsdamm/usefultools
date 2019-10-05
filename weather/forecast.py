76edd9a767f463c19a951f39b50a1d69dc8e7909
Renee Damm <renee@plesba.com>
2019-10-04 21:38:42 -0600

    added icons

--- weather/forecast.py
+++ weather/forecast.py
@@ -1,5 +1,6 @@
 import requests
 
+
 url = "https://api.weather.gov/gridpoints/BOU/45,66/forecast"
 
 response = requests.request("GET", url)
@@ -7,8 +8,31 @@ response = requests.request("GET", url)
 #print(response.text)
 data = response.json()
 
-periods = data['properties']['periods']
-print(len(periods))
+periods = data["properties"]["periods"]
+#print(len(periods))
+
+htmlpage = """
+<html><head><title>Rangeview Forecast</title></head>
+<body>
+<h2>Rangeview Forecast</h2>
+<hr/>
+<table border="1">
+<tr><th>HighTemp</th><th>LowTemp</th><th>PrecipChance</th><th>Descrip</th></tr>
+"""
 
 for i in periods:
-        print(i['shortForecast'])
+    htmlpage += "<tr>"
+    htmlpage += "<td>" + i['name'] + "</td>"
+    htmlpage += "<td>" + str(i['temperature']) + "</td>"
+    htmlpage += "<td>" + i['shortForecast'] + "</td>"
+    htmlpage += "<td>" + "<img src=" + i['icon'] + "></td>"
+    htmlpage += "</tr>\n"
+
+htmlpage += """
+</table>
+</body>
+</html>
+"""
+
+
+print(htmlpage)
