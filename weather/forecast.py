45050ec0f6ffd0fa04e1ed100dbd2e3591dd5272
Renee Damm <renee@plesba.com>
2019-10-23 10:39:13 -0600

    -gets short forecast and prints it

--- weather/forecast.py
+++ weather/forecast.py
@@ -1,18 +1,17 @@
 from botocore.vendored  import requests
 import boto3
 from botocore.exceptions import ClientError
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 #https://docs.aws.amazon.com/ses/latest/DeveloperGuide/send-using-sdk-python.html
 
 htmlpage = """
-<html><head><title>Rangeview Forecast</title></head>
+<html><head><title>Forecast</title></head>
 <body>
-<h2>Rangeview Forecast </h2>
+<h2>Black Hawk Rangeview Forecast </h2>
 <hr/>
-<table border="1">
-<tr><th>Day</th><th>Temp</th><th>Forecast</th><th>Icon</th></tr>
+<table border="1" width="645">
 """
 def main():
     weather_report()
@@ -28,15 +27,19 @@ def lambda_handler(event, context):
     }
 
 def send_email(htmlpage):
-    SENDER = "test@test.com"
-    RECIPIENT = "testto@test.com"
+    SENDER = "x@x.com"
+    RECIPIENT = "x@x.com"
     CONFIGURATION_SET = "ConfigSet"
     AWS_REGION = "us-west-2"
 
-    now = datetime.now()
+    subject_line_dt = datetime.now()
 
-    dt_string = now.strftime("%m/%d/%Y %H:%M")
-    subject_line= "Rangeview Weather Report - " + dt_string
+    #dt_string = subject_line_dt.strftime("%m/%d/%Y %H:%M")
+    #subject_line= "Black Hawk Rangeview Weather Forecast - " + dt_string
+
+    dt_string_local = subject_line_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+    dt_string = dt_string_local.strftime("%m/%d/%Y %H:%M")
+    subject_line= "Black Hawk Rangeview Weather Forecast "
 
     # The subject line for the email.
     SUBJECT = subject_line
@@ -101,64 +104,51 @@ def weather_report():
     data = response.json()
 
     periods = data["properties"]["periods"]
-    #print(len(periods))
 
-
+    #build the table header
     htmlpage += "<tr>"
-
     for i in periods:
         if i['isDaytime']:
-            htmlpage += "<td>" + i['name'] + "<br>" + "Hi " + str(i['temperature']) + "º" + "<br>" + i['shortForecast'] +"</td>"
-
-    #    htmlpage += "<td>" + str(i['temperature']) + "</td>"
-    #    htmlpage += "<td>" + i['shortForecast'] + "</td>"
-    #    htmlpage += "<td>" + "<img src=" + i['icon'] + "></td>"
+            htmlpage += "<th>" + i['name'] +"</th>"
 
     htmlpage += "</tr>\n"
 
+    #get the daytime information
+    htmlpage += "<tr>"
+    for i in periods:
+        if i['isDaytime']:
+            htmlpage += "<td word-wrap: break-word>" + "Hi " + str(i['temperature']) + "º" + "<br>" + i['shortForecast'] + "</td>"
+
+
+    htmlpage += "</tr>\n"
+
+    #get the daytime icons
+    htmlpage += "<tr>"
     for i in periods:
         if i['isDaytime']:
             htmlpage += "<td>" + "<img src=" + i['icon'] + "></td>"
-    #    htmlpage += "<td>" + str(i['temperature']) + "</td>"
-    #    htmlpage += "<td>" + i['shortForecast'] + "</td>"
-    #    htmlpage += "<td>" + "<img src=" + i['icon'] + "></td>"
 
     htmlpage += "</tr>\n"
 
-
+    #get the evening information
+    htmlpage += "<tr>"
     for i in periods:
         if not i['isDaytime']:
             htmlpage += "<td>" + "Lo " + str(i['temperature']) + "º" + "<br>" + i['shortForecast'] + "</td>"
 
-    #    htmlpage += "<td>" + str(i['temperature']) + "</td>"
-    #    htmlpage += "<td>" + i['shortForecast'] + "</td>"
-    #    htmlpage += "<td>" + "<img src=" + i['icon'] + "></td>"
-
     htmlpage += "</tr>\n"
 
+    #get the evening icons
+    htmlpage += "<tr>"
     for i in periods:
         if not i['isDaytime']:
             htmlpage += "<td>" + "<img src=" + i['icon'] + "></td>"
+
     #    htmlpage += "<td>" + str(i['temperature']) + "</td>"
     #    htmlpage += "<td>" + i['shortForecast'] + "</td>"
     #    htmlpage += "<td>" + "<img src=" + i['icon'] + "></td>"
 
     htmlpage += "</tr>\n"
-
-    #for i in periods:
-    #    htmlpage += "<tr>"
-    #    htmlpage += "<td>" + i['name'] + "</td>"
-    #    htmlpage += "<td>" + str(i['temperature']) + "</td>"
-    #    htmlpage += "<td>" + i['shortForecast'] + "</td>"
-    #    htmlpage += "<td>" + "<img src=" + i['icon'] + "></td>"
-    #   htmlpage += "</tr>\n"
-    #htmlpage += """
-    #</table>
-    #</body>
-    #</html>
-    #"""
-
-
 
     htmlpage += """
 </table>
